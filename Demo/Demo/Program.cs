3e008634fca4d6737645281a2cbb6e2d5968bbc6
nix <nhoxqd@gmail.com>
2024-01-26 18:39:34 +0700

    1

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 1;
+            Console.WriteLine(x);
         }
     }
 }
