f6c9b7689714bb33330c795c45ca952c537b99ef
nix <nhoxqd@gmail.com>
2024-01-26 15:44:01 +0700

    Duy

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            int y = 2;
             Console.WriteLine("Hello, World!");
+            Console.WriteLine(x + y);
         }
     }
 }
