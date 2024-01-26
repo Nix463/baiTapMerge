d110554e09ad3c094d5458d1cc944ca5cb70faee
nix <nhoxqd@gmail.com>
2024-01-26 20:51:26 +0700

    pt

--- Demo/Demo/Program.cs
+++ Demo/Demo/Program.cs
@@ -1,11 +1,20 @@
-﻿namespace Demo
+﻿using System.Net.Security;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            Console.WriteLine(x);
+            int x = 4;
+            
+            Console.Write("Nhap mot so nguyen: ");
+            int y = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Phuong trinh");
+            int A = x * x + y * y;
+            Console.WriteLine(A);
+            
         }
     }
 }
